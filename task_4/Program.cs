5f411fbdde54e59f9f5105edc13bb4ff17cd0ef9
Oleg_bul <kokseg63@gmail.com>
2022-09-18 20:54:49 +0400

    task_4 complite

---
+++ task_4/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите число а");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число c");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+else if (b < max)   
+{
+    max = a;
+}
+
+if (c > max)
+{
+    max = c;
+}
+else if (c < max)
+{
+    max = max;
+}
+
+Console.WriteLine("Максимальное число " + max);
