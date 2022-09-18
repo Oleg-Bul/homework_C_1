9eb1d0f058fdbc471aaf68cb71ee4f98107096de
Oleg_bul <kokseg63@gmail.com>
2022-09-18 22:27:47 +0400

    task_8 complite_fin

--- task_8/Program.cs
+++ task_8/Program.cs
@@ -3,17 +3,6 @@ int a = int.Parse(Console.ReadLine());
 int b = 0;
 while(b < a-2)
 {
-    if (a % 2 == 0)
-    {
-
-        Console.WriteLine(b+2);
-        b = b+2;
-        
-    }
-    else
-    {
-        Console.WriteLine(b+2);
-        b = b+2;
-        
-    }
+    Console.WriteLine(b+2);
+    b = b+2;
 }
