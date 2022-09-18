29450592ec417def762f33409e6934a4202db1bd
Oleg_bul <kokseg63@gmail.com>
2022-09-18 22:23:56 +0400

    task_8 complite

---
+++ task_8/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+int b = 0;
+while(b < a-2)
+{
+    if (a % 2 == 0)
+    {
+
+        Console.WriteLine(b+2);
+        b = b+2;
+        
+    }
+    else
+    {
+        Console.WriteLine(b+2);
+        b = b+2;
+        
+    }
+}
